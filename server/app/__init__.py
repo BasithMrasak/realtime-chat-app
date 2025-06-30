4a73aa0fbe144f191458045a22e096c746ed1be4
BasithMrasak <basithmrasak1@gmail.com>
2025-06-30 20:42:57 +0530

    Add gunicorn to requirements

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from pymongo import MongoClient
+import os
+
+client = MongoClient(os.environ["MONGO_URI"])
+db = client["chat_db"]
+
 
 # ✅ Create the SocketIO instance at the top
 socketio = SocketIO()
@@ -9,9 +14,9 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
 
-    # ✅ MongoDB connection
-    client = MongoClient("mongodb://localhost:27017/")
-    app.db = client["chat_db"]
+    # # ✅ MongoDB connection
+    # client = MongoClient("mongodb://localhost:27017/")
+    # app.db = client["chat_db"]
 
     # ✅ Register Blueprint
     from .routes import main
